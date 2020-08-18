327175886bbe8eb9bc292c7178dda601733be859
Alexandre Debusschère <zizilex@gmail.com>
2020-08-18 13:06:41 +0700

    Refactor: BodyParserMiddleware.php.

--- src/Middleware/BodyParserMiddleware.php
+++ src/Middleware/BodyParserMiddleware.php
@@ -15,13 +15,6 @@ use SimpleXMLElement;
 class BodyParserMiddleware implements MiddlewareInterface
 {
 
-    /** @var string[] */
-    protected $no_need_to_process_methods = [
-        'GET',
-        'HEAD',
-        'OPTIONS'
-    ];
-
     /**
      * @param ServerRequestInterface $request
      * @param RequestHandlerInterface $handler
@@ -29,48 +22,123 @@ class BodyParserMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (in_array($request->getMethod(), $this->no_need_to_process_methods)) {
+        if ($this->isNonBodyRequest($request)) {
             return $handler->handle($request);
         }
 
-        $content_type = strtolower(trim($request->getHeaderLine('Content-Type')));
-        if (!strlen($content_type)) {
-            return $handler->handle($request);
-        }
+        $content_type = $this->getContentType($request);
 
-        $content_type = explode(';', $content_type)[0];
-
-        switch ($content_type) {
-            case 'application/x-www-form-urlencoded':
-                $data = null;
-                parse_str((string)$request->getBody(), $data);
-                $request = $request->withParsedBody($data);
-                break;
-
-            case 'application/json':
-                $body = json_decode((string)$request->getBody(), true);
-                if (is_array($body)) {
-                    $request = $request->withParsedBody($body);
-                }
-                break;
-
-            case 'application/xml':
-            case 'text/xml':
-                $backup = libxml_disable_entity_loader(true);
-                $backup_errors = libxml_use_internal_errors(true);
-
-                $xml = simplexml_load_string((string)$request->getBody());
-
-                libxml_disable_entity_loader($backup);
-                libxml_clear_errors();
-                libxml_use_internal_errors($backup_errors);
-
-                if ($xml instanceof SimpleXMLElement) {
-                    $request = $request->withParsedBody($xml);
-                }
-                break;
+        if ($this->isUrlEncoded($content_type)) {
+            $request = $this->getUrlEncodedParsedBody($request);
+        } elseif ($this->isJson($content_type)) {
+            $request = $this->getJsonParsedBody($request);
+        } elseif ($this->isXML($content_type)) {
+            $request = $this->getXMLParsedBody($request);
         }
 
         return $handler->handle($request);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return bool
+     */
+    protected function isNonBodyRequest(ServerRequestInterface $request): bool
+    {
+        return in_array($request->getMethod(), [
+            'GET',
+            'HEAD',
+            'OPTIONS'
+        ]);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return string
+     */
+    protected function getContentType(ServerRequestInterface $request): string
+    {
+        $content_type = strtolower(trim($request->getHeaderLine('Content-Type')));
+        $content_type = explode(';', $content_type)[0];
+
+        return $content_type;
+    }
+
+    /**
+     * @param string $content_type
+     * @return bool
+     */
+    protected function isUrlEncoded(string $content_type): bool
+    {
+        return $content_type == 'application/x-www-form-urlencoded';
+    }
+
+    /**
+     * @param string $content_type
+     * @return bool
+     */
+    protected function isJson(string $content_type): bool
+    {
+        return $content_type == 'application/json';
+    }
+
+    /**
+     * @param string $content_type
+     * @return bool
+     */
+    protected function isXML(string $content_type): bool
+    {
+        return in_array($content_type, [
+            'application/xml',
+            'text/xml'
+        ]);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ServerRequestInterface
+     */
+    protected function getUrlEncodedParsedBody(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $data = null;
+        parse_str((string)$request->getBody(), $data);
+
+        return $request->withParsedBody($data);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ServerRequestInterface
+     */
+    protected function getJsonParsedBody(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $body = json_decode((string)$request->getBody(), true);
+        if (is_array($body)) {
+            $request = $request->withParsedBody($body);
+        }
+
+        return $request;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ServerRequestInterface
+     */
+    protected function getXMLParsedBody(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $backup = libxml_disable_entity_loader(true);
+        $backup_errors = libxml_use_internal_errors(true);
+
+        $xml = simplexml_load_string((string)$request->getBody());
+
+        libxml_disable_entity_loader($backup);
+        libxml_clear_errors();
+        libxml_use_internal_errors($backup_errors);
+
+        if ($xml instanceof SimpleXMLElement) {
+            $request = $request->withParsedBody($xml);
+        }
+
+        return $request;
     }
 }
