e9b2d2f88fd01c18ebe94bc06ecb8439b380ff7f
Alexandre Debusschère <zizilex@gmail.com>
2020-09-24 17:13:39 +0700

    FEAT: Added cache capabilities to environment.

--- bootstrap/env.php
+++ bootstrap/env.php
@@ -2,5 +2,21 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+$environment_file = __DIR__.'/../config/environment.php';
+
+// If environment file exists, load it so that it wont be necessary to parse and load .env file at every call.
+if (file_exists($environment_file)) {
+    $environment = require_once $environment_file;
+    $_ENV = array_merge($_ENV, $environment);
+    return;
+}
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
-$dotenv->load();
+$environment = $dotenv->load();
+
+// In production, save the environment in a file so that it wont be necessary to parse and load .env file at every call.
+if (env('APP_ENV') == 'production') {
+    $file = new SplFileObject($environment_file, 'w');
+    $file->fwrite('<?php'.PHP_EOL.PHP_EOL.'return '.var_export($environment, true).';'.PHP_EOL);
+    $file = null;
+}
