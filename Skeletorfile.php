bf9172166d7773b98a8a58e59397d41dfc9d48e5
Alexandre Debusschère <alexandre.debusschere@hey.com>
2025-04-23 21:19:13 +0200

    upd: Skeletorfile.php

--- Skeletorfile.php
+++ Skeletorfile.php
@@ -5,6 +5,8 @@ use NiftyCo\Skeletor\Skeletor;
 return function (Skeletor $skeletor) {
     $skeletor->intro('Installation wizard');
 
+    $app_name = $skeletor->text('What is the name of your application ?', 'Borsch-Skeleton');
+
     $installation_type = $skeletor->select('What type of application do you need ?', [
         'MINIMAL' => 'Minimal (to create API)',
         'FULL' =>    'Full    (with views)',
@@ -54,6 +56,19 @@ return function (Skeletor $skeletor) {
 
             return true;
         }, 'Removing Latte template engine from composer.json', 'Unable to completely remove Latte template engine from composer.json');
+
+        $skeletor->spin('Creating environment file', function () use ($skeletor, $app_name) {)
+            if (!$skeletor->exists('.env')) {
+                copy('.env.example', '.env');
+            }
+
+            $random_key = base64_encode(random_bytes(32));
+
+            $skeletor->replaceInFile('APP_NAME=Laravel', 'APP_NAME='.$app_name, '.env');
+            $skeletor->replaceInFile('APP_KEY=', 'APP_KEY='.$random_key, '.env');
+
+            return true;
+        }, 'Created environment file', 'Unable to create environment file');
     }
 
     $skeletor->outro('Installation complete');
