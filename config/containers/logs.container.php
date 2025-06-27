8f8c24272897f8db4b3e8207ecae5abb0d226d3e
Alexandre Debusschère <alexandre.debusschere@hey.com>
2025-06-27 20:17:45 +0200

    clean: simplified logger definition

--- config/containers/logs.container.php
+++ config/containers/logs.container.php
@@ -4,32 +4,19 @@ use Monolog\{Handler\StreamHandler, Level, Logger, Processor\PsrLogMessageProces
 use League\Container\{Container, ServiceProvider\AbstractServiceProvider};
 
 return static function(Container $container): void {
-    $container->addServiceProvider(new class extends AbstractServiceProvider {
+    $container->add(Logger::class, function (): Logger {
+        $name = env('APP_NAME', 'App');
 
-        public function provides(string $id): bool
-        {
-            return in_array($id, [
-                Logger::class,
-            ]);
-        }
+        $handlers = [
+            new StreamHandler(
+                logs_path(env('LOG_CHANNEL', 'app').'.log'),
+                Level::fromName(env('LOG_LEVEL', 'Debug'))
+            )
+        ];
 
-        public function register(): void
-        {
-            $this->getContainer()->add(Logger::class, function (): Logger {
-                $name = env('APP_NAME', 'App');
+        $processors = [new PsrLogMessageProcessor(removeUsedContextFields: true)];
+        $datetime_zone = new DateTimeZone(env('TIMEZONE', 'UTC'));
 
-                $handlers = [
-                    new StreamHandler(
-                        logs_path(env('LOG_CHANNEL', 'app').'.log'),
-                        constant(Level::class.'::'.ucfirst(env('LOG_LEVEL', 'Debug')))
-                    )
-                ];
-
-                $processors = [new PsrLogMessageProcessor(removeUsedContextFields: true)];
-                $datetime_zone = new DateTimeZone(env('TIMEZONE', 'UTC'));
-
-                return new Logger($name, $handlers, $processors, $datetime_zone);
-            });
-        }
+        return new Logger($name, $handlers, $processors, $datetime_zone);
     });
 };
